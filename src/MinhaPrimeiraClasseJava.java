3854ed9ee5ef19738264f7c4c89d65f7da0f9f87
guest01 <guest01@FLE207D15.alunos.sc.senac.br>
2022-06-22 20:45:41 -0300

    Apenas OK

--- src/MinhaPrimeiraClasseJava.java
+++ src/MinhaPrimeiraClasseJava.java
@@ -4,6 +4,7 @@ public class MinhaPrimeiraClasseJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Olá");
+System.out.println("Ok");
 	}
 
 }
